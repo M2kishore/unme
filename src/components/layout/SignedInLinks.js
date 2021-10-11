b63f9639870e0dceb8a9998fe6219a54ed5780c1
M M Kishore <kishoremeenakshi2001@gmail.com>
2021-10-11 19:13:07 +0530

    added logout function and mapped to logout button

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import { logout } from '../../firebase/Firebase';
 const SignedInLinks = () => {
   return (
     <div>
       <ul className="right hide-on-med-and-down">
         <li><NavLink to='/create' className="grey-text text-darken-4" activeClassName="teal lighten-2">New Memory</NavLink></li>
         <li><NavLink to='/whereabout' className="grey-text text-darken-4" activeClassName="teal lighten-2">Whereabouts</NavLink></li>
-        <li><NavLink to='/' exact className="grey-text text-darken-4" activeClassName="teal lighten-2">Log Out</NavLink></li>
+        <li><NavLink to='/' exact className="grey-text text-darken-4" activeClassName="teal lighten-2" isActive={logout}>Log Out</NavLink></li>
         <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
       </ul>
       <ul className="sidenav" id="mobile-nav">
